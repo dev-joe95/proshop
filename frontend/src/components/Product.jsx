6df454a7d3a10abc33b12ea0899de35d2a8bea39
Dev Joe <joe.afify95@gmail.com>
2020-12-23 08:16:04 +0200

    Adding router links to product component

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 import { Card } from "react-bootstrap";
-import Rating from './Rating';
+import Rating from "./Rating";
+import { NavLink } from "react-router-dom";
 
 const Product = ({ product }) => {
     const { _id, name, image, rating, numReviews, price } = product;
     return (
         <Card className="my-3 p-3 rounded">
-            <a href={`/product/${_id}`}>
+            <NavLink to={`/product/${_id}`}>
                 <Card.Img src={image} variant="top" />
-            </a>
+            </NavLink>
             <Card.Body>
-                <a href={`/product/${_id}`}>
+                <NavLink to={`/product/${_id}`}>
                     <Card.Title as="div">{name}</Card.Title>
-                </a>
+                </NavLink>
                 <Card.Text as="div">
                     <Rating value={rating} text={`${numReviews} reviews`} />
                 </Card.Text>
