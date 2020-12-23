c21dcb5de1f36f1960b7968c452558fd63b78515
Dev Joe <joe.afify95@gmail.com>
2020-12-23 08:06:00 +0200

    Importing routers in App component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Container } from "react-bootstrap";
+import { BrowserRouter, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
@@ -8,13 +9,16 @@ import HomeScreen from "./screens/HomeScreen";
 function App() {
     return (
         <React.Fragment>
-            <Header />
-            <Container>
-                <main className="py-4">
-                    <HomeScreen />
-                </main>
-            </Container>
-            <Footer />
+            <BrowserRouter>
+                <Header />
+                <Container>
+                    <main className="py-4">
+                        <Route path="/" component={HomeScreen} exact/>
+                        
+                    </main>
+                </Container>
+                <Footer />
+            </BrowserRouter>
         </React.Fragment>
     );
 }
