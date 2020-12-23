b72ecf63be5874eb1513ed831a0fa029bda093f8
Dev Joe <joe.afify95@gmail.com>
2020-12-23 08:30:16 +0200

    Adding router links to header component

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,58 +1,80 @@
 import React from "react";
 import { Navbar, Nav, NavDropdown, Container } from "react-bootstrap";
 import logo from "../logo.svg";
+import { LinkContainer } from "react-router-bootstrap";
 
 const Header = () => {
     return (
         <header>
             <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
                 <Container>
-                    <Navbar.Brand href="/">
-                        <img
-                            alt=""
-                            src={logo}
-                            width="20"
-                            height="20"
-                            className="d-inline-block align-top"
-                        />{" "}
-                        proshop
-                    </Navbar.Brand>
+                    <LinkContainer to="/">
+                        <Navbar.Brand>
+                            <img
+                                alt=""
+                                src={logo}
+                                width="20"
+                                height="20"
+                                className="d-inline-block align-top"
+                            />{" "}
+                            proshop
+                        </Navbar.Brand>
+                    </LinkContainer>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="mr-auto">
-                            <Nav.Link href="#features">products</Nav.Link>
-                            <Nav.Link href="#pricing">categories</Nav.Link>
+                            <LinkContainer to="/products">
+                                <Nav.Link>products</Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer to="/categories">
+                                <Nav.Link>categories</Nav.Link>
+                            </LinkContainer>
                             <NavDropdown
                                 title="categories"
                                 id="collasible-nav-dropdown"
-                                className="text-uppercase"
                             >
-                                <NavDropdown.Item href="#action/3.1">
-                                    category 1
-                                </NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.2">
-                                    category 2
-                                </NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">
-                                    category 3
-                                </NavDropdown.Item>
+                                <LinkContainer to="/category-1">
+                                    <NavDropdown.Item>
+                                        category 1
+                                    </NavDropdown.Item>
+                                </LinkContainer>
+                                <LinkContainer to="/category-2">
+                                    <NavDropdown.Item>
+                                        category 2
+                                    </NavDropdown.Item>
+                                </LinkContainer>
+                                <LinkContainer to="/category-3">
+                                    <NavDropdown.Item>
+                                        category 3
+                                    </NavDropdown.Item>
+                                </LinkContainer>
                                 <NavDropdown.Divider />
-                                <NavDropdown.Item href="#action/3.4">
-                                    category 1
-                                </NavDropdown.Item>
+                                <LinkContainer to="/categories">
+                                    <NavDropdown.Item>
+                                        categories
+                                    </NavDropdown.Item>
+                                </LinkContainer>
                             </NavDropdown>
                         </Nav>
                         <Nav>
-                            <Nav.Link href="/cart">
-                                <i className="fas fa-shopping-cart px-1"></i>
-                                cart
-                            </Nav.Link>
-                            <Nav.Link href="/signup">
-                                <i className="fas fa-user-plus px-1"></i>register
-                            </Nav.Link>
-                            <Nav.Link eventKey={2} href="/signin">
-                                <i className="fas fa-sign-in-alt px-1"></i>login
-                            </Nav.Link>
+                            <LinkContainer to="/cart">
+                                <Nav.Link>
+                                    <i className="fas fa-shopping-cart px-1"></i>
+                                    cart
+                                </Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer to="/signup">
+                                <Nav.Link>
+                                    <i className="fas fa-user-plus px-1"></i>
+                                    register
+                                </Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer to="/login">
+                                <Nav.Link>
+                                    <i className="fas fa-sign-in-alt px-1"></i>
+                                    login
+                                </Nav.Link>
+                            </LinkContainer>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
