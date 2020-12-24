44488b4c667f229b42b6fc593b623d6cd5853095
Dev Joe <joe.afify95@gmail.com>
2020-12-24 20:08:34 +0200

    Removing routes from seperated routes from index.js

--- backend/index.js
+++ backend/index.js
@@ -1,28 +1,19 @@
 import express from "express";
-import products from "./data/products.js";
 import colors from "colors";
 import dotenv from "dotenv";
 import db from "./startup/db.js";
+import routes from "./startup/routes.js";
 
 dotenv.config();
-
-// Calling mongodb connetion
+/**
+ * Calling mongodb connetion
+ */
 db();
-
 const app = express();
-
-app.get("/", (req, res) => {
-    res.send("Hello from Proshop application");
-});
-
-app.get("/api/product", (req, res) => {
-    res.json(products);
-});
-
-app.get("/api/product/:id", (req, res) => {
-    const product = products.find((p) => p._id === req.params.id);
-    res.json(product);
-});
+/**
+ * Calling app routes
+ */
+routes(app);
 
 const port = process.env.PORT || 5000;
 app.listen(port, console.log(`Server running on port ${port}`.bgGreen.black));
