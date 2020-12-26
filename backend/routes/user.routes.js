ed984bfbc4eea12492834677a147833c4a0e0968
Dev Joe <joe.afify95@gmail.com>
2020-12-26 05:48:59 +0200

    Adding getUserProfile to user routes

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,13 +1,19 @@
 import express from "express";
-import { authUser } from "../controllers/user.controller.js";
+import { authUser, getUserProfile } from "../controllers/user.controller.js";
+import protect from "../middleware/auth.js";
 
 const router = express.Router();
 
 /**
- * @url         /api/user/
+ * @url         /api/user/login
  * @access      public
  */
 
 router.post("/login", authUser);
+/**
+ * @url         /api/user/me
+ * @access      private
+ */
+router.get("/me", protect, getUserProfile);
 
 export default router;
