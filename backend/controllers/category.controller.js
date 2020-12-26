1169ae755650e35b21195a3d51727e2d54c14392
Dev Joe <joe.afify95@gmail.com>
2020-12-26 02:17:18 +0200

    Adding asyncHandler for category & product controllers

--- backend/controllers/category.controller.js
+++ backend/controllers/category.controller.js
@@ -1,18 +1,18 @@
-// import mongoose from"mongoose";
+import asyncHandler from "express-async-handler";
 import Category from "../models/category.js";
 
-const getAll = async (req, res) => {
+const getAll = asyncHandler(async (req, res) => {
     await Category.find({}, (err, category) => {
         if (err) res.status(400).send(err);
         res.json(category);
     }).sort("name");
-};
+});
 
-const getById = async (req, res) => {
+const getById = asyncHandler(async (req, res) => {
     await Category.findById(req.params.id, (err, category) => {
         if (err) res.status(400).send(err);
         res.json(category);
     });
-};
+});
 
 export { getAll, getById };
--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,18 +1,18 @@
-// import mongoose from"mongoose";
+import asyncHandler from "express-async-handler";
 import Product from "../models/product.js";
 
-const getAll = async (req, res) => {
+const getAll = asyncHandler(async (req, res) => {
     await Product.find({}, (err, product) => {
         if (err) res.status(400).send(err);
         res.json(product);
     }).sort("name");
-};
+});
 
-const getById = async (req, res) => {
+const getById = asyncHandler(async (req, res) => {
     await Product.findById(req.params.id, (err, product) => {
         if (err) res.status(400).send(err);
         res.json(product);
     });
-};
+})
 
 export { getAll, getById };
