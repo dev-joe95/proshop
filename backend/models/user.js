5bbae1b74a422fd6577357b1984afae8a02a0c22
Dev Joe <joe.afify95@gmail.com>
2020-12-26 06:21:04 +0200

    Adding bcrypt password middleware to user model

--- backend/models/user.js
+++ backend/models/user.js
@@ -39,6 +39,14 @@ userSchema.methods.generateToken = function () {
         }
     );
 };
+userSchema.pre("save", async function (next) {
+    if (this.isModified("password")) {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+    } else {
+        next();
+    }
+});
 const User = mongoose.model("User", userSchema);
 
 export default User;
