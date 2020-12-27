f9d4693dea195b1e7edfdc8265e2d96bcc125dc6
Dev Joe <joe.afify95@gmail.com>
2020-12-27 08:19:34 +0200

    Adding user register reducer

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -12,3 +12,15 @@ export const userLoginReducer = (state = {}, action) => {
             return state;
     }
 };
+export const userRegisterReducer = (state = {}, action) => {
+    switch (action.type) {
+        case "USER_REGISTER_REQUEST":
+            return { loading: true };
+        case "USER_REGISTER_SUCCESS":
+            return { loading: false, token: action.payload };
+        case "USER_REGISTER_FAIL":
+            return { loading: false, errors: action.payload };
+        default:
+            return state;
+    }
+};
