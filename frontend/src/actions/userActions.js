1a230c43965e0d5ab548909d0487e0c6d81a664c
Dev Joe <joe.afify95@gmail.com>
2020-12-27 08:27:58 +0200

    Adding user register action

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -29,10 +29,39 @@ export const login = (email, password) => async (dispatch) => {
         });
     }
 };
-
+export const register = (name, email, password) => async (dispatch) => {
+    try {
+        dispatch({
+            type: "USER_REGISTER_REQUEST",
+        });
+        const config = {
+            headers: {
+                "Content-Type": "application/json",
+            },
+        };
+        const { data } = await axios.post(
+            "/api/user/",
+            { name, email, password },
+            config
+        );
+        dispatch({
+            type: "USER_REGISTER_SUCCESS",
+            payload: data,
+        });
+        localStorage.setItem("token", data.token);
+    } catch (error) {
+        dispatch({
+            type: "USER_REGISTER_FAIL",
+            payload:
+                error.response && error.response.data.message
+                    ? error.response.data.message
+                    : error.message,
+        });
+    }
+};
 export const logout = () => async (dispatch) => {
     localStorage.removeItem("token");
     dispatch({
-        type: "USER_LOGOUT"
+        type: "USER_LOGOUT",
     });
 };
