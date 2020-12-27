0394120863437ed119cdcd2980857e539184887e
Dev Joe <joe.afify95@gmail.com>
2020-12-27 08:17:53 +0200

    Modifying register route to return only token

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -38,10 +38,6 @@ const registerUser = asyncHandler(async (req, res) => {
     if (user) {
         res.status(201);
         res.json({
-            _id: user._id,
-            name: user.name,
-            email: user.email,
-            isAdmin: user.isAdmin,
             token: user.generateToken(),
         });
     } else {
