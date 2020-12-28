08b7a1e77008423127cd8eadb6280775f94cebc9
Dev Joe <joe.afify95@gmail.com>
2020-12-28 12:46:52 +0200

    Adding profile screen route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
 import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
+import ProfileScreen from "./screens/ProfileScreen";
 
 function App() {
     return (
@@ -17,6 +18,7 @@ function App() {
                 <Header />
                 <Container>
                     <main className="py-4">
+                        <Route path="/profile" component={ProfileScreen} />
                         <Route path="/register" component={RegisterScreen} />
                         <Route path="/login" component={LoginScreen} />
                         <Route path="/cart/:id?" component={CartScreen} />
