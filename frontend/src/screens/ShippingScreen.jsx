d95436c0a29b4e8a885a83f319bff82bcf413489
Dev Joe <joe.afify95@gmail.com>
2020-12-30 13:02:50 +0200

    Connecting with redux store

--- frontend/src/screens/ShippingScreen.jsx
+++ frontend/src/screens/ShippingScreen.jsx
@@ -3,15 +3,20 @@ import { Form, Button } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import Loader from "../components/Loader";
 import FormContainer from "../components/FormContainer";
+import { saveShippingAddress } from "../actions/cartActions";
 
 const ShippingScreen = ({ history }) => {
-    const [address, setAddress] = useState("");
-    const [city, setCity] = useState("");
-    const [postalCode, setPostalCode] = useState("");
-    const [country, setCountry] = useState("");
+    const { shippingAddress } = useSelector((state) => state.cart);
+    const [address, setAddress] = useState(shippingAddress.address);
+    const [city, setCity] = useState(shippingAddress.city);
+    const [postalCode, setPostalCode] = useState(shippingAddress.postalCode);
+    const [country, setCountry] = useState(shippingAddress.country);
+    const dispatch = useDispatch();
     const submitHandler = (e) => {
         e.preventDefault();
-    };  
+        dispatch(saveShippingAddress({ address, city, postalCode, country }));
+        history.push("/payment");
+    };
     return (
         <FormContainer>
             <h1>Shipping</h1>
@@ -20,7 +25,7 @@ const ShippingScreen = ({ history }) => {
                     <Form.Label>Address</Form.Label>
                     <Form.Control
                         type="text"
-                        placeholder="Example:  John Ash"
+                        placeholder="Example:  13 New Horizon Street"
                         value={address}
                         onChange={(e) => setAddress(e.target.value)}
                         autoFocus
@@ -31,7 +36,7 @@ const ShippingScreen = ({ history }) => {
                     <Form.Label>City</Form.Label>
                     <Form.Control
                         type="text"
-                        placeholder="Example:  John Ash"
+                        placeholder="Example:  New York"
                         value={city}
                         required
                         onChange={(e) => setCity(e.target.value)}
@@ -41,7 +46,7 @@ const ShippingScreen = ({ history }) => {
                     <Form.Label>Postal code</Form.Label>
                     <Form.Control
                         type="text"
-                        placeholder="Example:  John Ash"
+                        placeholder="Example:  10935"
                         value={postalCode}
                         required
                         onChange={(e) => setPostalCode(e.target.value)}
@@ -51,13 +56,15 @@ const ShippingScreen = ({ history }) => {
                     <Form.Label>Country</Form.Label>
                     <Form.Control
                         type="text"
-                        placeholder="Example:  John Ash"
+                        placeholder="Example:  United Status"
                         value={country}
                         required
                         onChange={(e) => setCountry(e.target.value)}
                     ></Form.Control>
                 </Form.Group>
-                <Button variant="dark">Continue</Button>
+                <Button variant="dark" type="submit">
+                    Continue
+                </Button>
             </Form>
         </FormContainer>
     );
