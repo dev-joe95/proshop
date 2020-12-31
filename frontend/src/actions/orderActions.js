6755cc329aec5886c76219189797432d8e99008e
Dev Joe <joe.afify95@gmail.com>
2020-12-31 18:18:45 +0200

    Creating getOrderDetails action

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -25,3 +25,29 @@ export const createOrder = (order) => async (dispatch, getState) => {
         });
     }
 };
+
+export const getOrderDetails = (id) => async (dispatch, getState) => {
+    try {
+        dispatch({
+            type: "ORDER_DETAILS_REQUEST",
+        });
+        const config = {
+            headers: {
+                Authorization: `Bearer ${getState().userLogin.token}`,
+            },
+        };
+        const { data } = await axios.get(`/api/order/${id}`, config);
+        dispatch({
+            type: "ORDER_DETAILS_SUCCESS",
+            payload: data,
+        });
+    } catch (error) {
+        dispatch({
+            type: "ORDER_DETAILS_FAIL",
+            payload:
+                error.response && error.response.data.message
+                    ? error.response.data.message
+                    : error.message,
+        });
+    }
+};
