5ee04c015f0e1a82d6877124d7dfca068bf1958a
Dev Joe <joe.afify95@gmail.com>
2021-01-02 01:28:23 +0200

    Adding payOrder action

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -51,3 +51,38 @@ export const getOrderDetails = (id) => async (dispatch, getState) => {
         });
     }
 };
+
+export const payOrder = (orderId, paymentResult) => async (
+    dispatch,
+    getState
+) => {
+    try {
+        dispatch({
+            type: "ORDER_PAY_REQUEST",
+        });
+        const config = {
+            headers: {
+                "Content-Type": "application/json",
+
+                Authorization: `Bearer ${getState().userLogin.token}`,
+            },
+        };
+        const { data } = await axios.put(
+            `/api/order/${orderId}/pay`,
+            paymentResult,
+            config
+        );
+        dispatch({
+            type: "ORDER_PAY_SUCCESS",
+            payload: data,
+        });
+    } catch (error) {
+        dispatch({
+            type: "ORDER_PAY_FAIL",
+            payload:
+                error.response && error.response.data.message
+                    ? error.response.data.message
+                    : error.message,
+        });
+    }
+};
