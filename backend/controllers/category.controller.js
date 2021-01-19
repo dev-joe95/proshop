d0723f4d894e823b1da8f4299549e210b933b068
Youssef Afify <joe.afify95@gmail.com>
2021-01-19 13:49:13 +0200

    Adding deleteCategory to category controller

--- backend/controllers/category.controller.js
+++ backend/controllers/category.controller.js
@@ -9,10 +9,24 @@ const getCategoryList = asyncHandler(async (req, res) => {
 });
 
 const getCategoryDetails = asyncHandler(async (req, res) => {
-    await Category.findById(req.params.id, (err, category) => {
-        if (err) res.status(400).send(err);
-        res.json(category);
-    });
-});
+    const category = await Category.findById(req.params.id);
 
+    if (category) {
+        res.json(category);
+    } else {
+        res.status(400);
+        throw new Error("Category not found");
+    }
+});
+const deleteCategory = asyncHandler(async (req, res) => {
+    const category = await Category.findById(req.params.id);
+    if (category) {
+        await category.remove();
+
+        res.json({ message: "Category successfully deleted" });
+    } else {
+        res.status(404);
+        throw new Error("Category not found");
+    }
+});
 export { getCategoryList, getCategoryDetails };
