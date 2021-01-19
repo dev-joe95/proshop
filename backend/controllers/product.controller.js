31cee7ed9953ee3c07db702ce8945823e151c5ac
Youssef Afify <joe.afify95@gmail.com>
2021-01-19 12:41:00 +0200

    Adding population to getProductList

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -5,7 +5,10 @@ const getProductList = asyncHandler(async (req, res) => {
     await Product.find({}, (err, product) => {
         if (err) res.status(400).send(err);
         res.json(product);
-    }).sort("-updatedAt");
+    })
+        .sort("-updatedAt")
+        .populate("user")
+        .populate("category");
 });
 
 const getProductDetails = asyncHandler(async (req, res) => {
