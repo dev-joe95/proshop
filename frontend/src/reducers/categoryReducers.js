b2b7224638ea8b29a2c521aef192fef0e9bcb47c
Youssef Afify <joe.afify95@gmail.com>
2021-01-19 23:43:00 +0200

    Adding categoryCreateReducer

--- frontend/src/reducers/categoryReducers.js
+++ frontend/src/reducers/categoryReducers.js
@@ -36,3 +36,18 @@ export const categoryDeleteReducer = (state = {}, action) => {
             return state;
     }
 };
+
+export const categoryCreateReducer = (state = { category: {} }, action) => {
+    switch (action.type) {
+        case "CATEGORY_CREATE_REQUEST":
+            return { ...state, loading: true };
+        case "CATEGORY_CREATE_SUCCESS":
+            return { loading: false, success: true, category: action.payload };
+        case "CATEGORY_CREATE_FAIL":
+            return { loading: false, error: action.payload };
+        case "CATEGORY_CREATE_RESET":
+            return {};
+        default:
+            return state;
+    }
+};
