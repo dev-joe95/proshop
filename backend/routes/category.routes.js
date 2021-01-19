0bb5848461e9e57657d4e756f62a20d1a9605d3b
Youssef Afify <joe.afify95@gmail.com>
2021-01-19 20:45:17 +0200

    Adding createCategory & updateCategory category routes

--- backend/routes/category.routes.js
+++ backend/routes/category.routes.js
@@ -3,6 +3,8 @@ import {
     getCategoryList,
     getCategoryDetails,
     deleteCategory,
+    createCategory,
+    updateCategory,
 } from "../controllers/category.controller.js";
 import { admin, protect } from "../middleware/auth.js";
 
@@ -28,5 +30,18 @@ router.get("/:id", getCategoryDetails);
  * @access      private/Admin
  */
 router.delete("/:id", protect, admin, deleteCategory);
-
+/**
+ * @description Create categories by admin
+ * @method      POST
+ * @url         /api/category/
+ * @access      private/Admin
+ */
+router.post("/", protect, admin, createCategory);
+/**
+ * @description Update categories by admin
+ * @method      PUT
+ * @url         /api/category/<id>
+ * @access      private/Admin
+ */
+router.put("/:id", protect, admin, updateCategory);
 export default router;
