6223afb26b6e3fa6918c32d481c36a9c98b743d3
Youssef Afify <joe.afify95@gmail.com>
2021-01-19 13:06:56 +0200

    Adding deleteProduct to product route

--- backend/routes/product.routes.js
+++ backend/routes/product.routes.js
@@ -2,7 +2,9 @@ import express from "express";
 import {
     getProductList,
     getProductDetails,
+    deleteProduct,
 } from "../controllers/product.controller.js";
+import { admin, protect } from "../middleware/auth.js";
 
 const router = express.Router();
 
@@ -10,8 +12,25 @@ const router = express.Router();
  * @url         /api/product/
  * @access      public
  */
-
+/**
+ * @description Get product for admin
+ * @method      GET
+ * @url         /api/product/
+ * @access      private/Admin
+ */
 router.get("/", getProductList);
+/**
+ * @description GET user by id for admin
+ * @method      GET
+ * @url         /api/product/<id>
+ * @access      private/Admin
+ */
 router.get("/:id", getProductDetails);
-
+/**
+ * @description Delete products for admin
+ * @method      DELETE
+ * @url         /api/product/<id>
+ * @access      private/Admin
+ */
+router.delete("/:id", protect, admin, deleteProduct);
 export default router;
