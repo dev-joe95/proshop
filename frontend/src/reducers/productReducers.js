238a08daa2af212a41d52b1e36d27134c319ca31
Youssef Afify <joe.afify95@gmail.com>
2021-01-20 02:50:29 +0200

    Adding productUpdateReducer

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -6,6 +6,8 @@ export const productListReducer = (state = { products: [] }, action) => {
             return { loading: false, products: action.payload };
         case "PRODUCT_LIST_FAIL":
             return { loading: false, error: [] };
+        case "PRODUCT_LIST_RESET":
+            return { products: [] };
         default:
             return state;
     }
@@ -49,7 +51,22 @@ export const productCreateReducer = (state = { product: {} }, action) => {
         case "PRODUCT_CREATE_FAIL":
             return { loading: false, error: action.payload };
         case "PRODUCT_CREATE_RESET":
-            return {};
+            return { product: {} };
+        default:
+            return state;
+    }
+};
+
+export const productUpdateReducer = (state = { product: {} }, action) => {
+    switch (action.type) {
+        case "PRODUCT_UPDATE_REQUEST":
+            return { loading: true };
+        case "PRODUCT_UPDATE_SUCCESS":
+            return { loading: false, success: true, product: action.payload };
+        case "PRODUCT_UPDATE_FAIL":
+            return { loading: false, error: action.payload };
+        case "PRODUCT_UPDATE_RESET":
+            return { product: {} };
         default:
             return state;
     }
