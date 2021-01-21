37538fefc86aed1e5fc61facb2fd5b8ee3e27ae1
Youssef Afify <joe.afify95@gmail.com>
2021-01-21 19:24:36 +0200

    Adding static uploads folder

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import colors from "colors";
+import path from "path";
 import dotenv from "dotenv";
 import db from "./startup/db.js";
 import routes from "./startup/routes.js";
@@ -14,6 +15,13 @@ const app = express();
  * Calling app routes
  */
 routes(app);
+
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+app.get("/", (req, res) => {
+    res.send("API is running");
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, console.log(`Server running on port ${port}`.bgGreen.black));
