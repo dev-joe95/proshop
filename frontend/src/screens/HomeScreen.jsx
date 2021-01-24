016162d9a6af8854b7c16dd0670d38d4ad7d3084
Youssef Afify <joe.afify95@gmail.com>
2021-01-24 07:15:08 +0200

    Adding Paginate component to HomeScreen

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -4,16 +4,18 @@ import { Alert, Col, Row } from "react-bootstrap";
 import Product from "../components/Product";
 import Loader from "../components/Loader";
 import { listProducts } from "../actions/productActions";
+import Paginate from "../components/Paginate";
 
 const HomeScreen = ({ match }) => {
     const keyword = match.params.keyword;
+    const pageNumber = match.params.pageNumber || 1;
     const dispatch = useDispatch();
-    const { loading, products, errors } = useSelector(
+    const { loading, products, errors, pages, page } = useSelector(
         (state) => state.productList
     );
     useEffect(() => {
-        dispatch(listProducts(keyword));
-    }, [dispatch, keyword]);
+        dispatch(listProducts(keyword, pageNumber, 8));
+    }, [dispatch, keyword, pageNumber]);
     return (
         <React.Fragment>
             {loading ? (
@@ -30,6 +32,11 @@ const HomeScreen = ({ match }) => {
                             </Col>
                         ))}
                     </Row>
+                    <Paginate
+                        pages={pages}
+                        page={page}
+                        keyword={keyword ? keyword : ""}
+                    />
                 </React.Fragment>
             )}
         </React.Fragment>
